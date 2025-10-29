e18e8e4abb31d21b6975c811d52ed1fc71a0ec44
berol24 <berolbertindjomo@gmail.com>
2025-10-29 16:55:34 +0100

    arrangement des logs d'envoi de mail

--- backend/Routes/SendEmailLogin.js
+++ backend/Routes/SendEmailLogin.js
@@ -1,11 +1,18 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: 'smtp.gmail.com',
+  port: 587,        
+  secure: false,    
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS,  
   },
+  tls: {
+    rejectUnauthorized: false  
+  },
+  logger: true,
+  debug: true,
 });
 
 export async function sendLoginEmail(email) {
