3c671560542037d8acd719a25e9bab0b0cfe734e
bertino2 <berolbertindjomo@gmail.com>
2025-10-30 20:35:41 +0100

    mise en place de l'envoi mail

--- backend/Routes/SendEmailLogin.js
+++ backend/Routes/SendEmailLogin.js
@@ -3,13 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  logger: true,
-  debug: true,
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
+    },
+    
+    tls: {
+        ciphers:'SSLv3' 
+    }
 });
 
 
