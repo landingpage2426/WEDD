ebd856367ec000676fe114457c4f951492876133
bertino2 <berolbertindjomo@gmail.com>
2025-10-30 21:21:08 +0100

    verification envoi email

--- backend/Routes/SendEmailLogin.js
+++ backend/Routes/SendEmailLogin.js
@@ -46,7 +46,7 @@ dotenv.config();
 // 1. Initialisez le client API avec votre clé
 const defaultClient = SibApiV3Sdk.ApiClient.instance;
 const apiKey = defaultClient.authentications['api-key'];
-apiKey.apiKey = process.env.BREVO_API_KEY;
+apiKey.apiKey = process.env.BREVO_KEY;
 
 const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
 
