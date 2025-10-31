6ff2360644dc173f15e844a61ae11b592be2b6fa
berol24 <berolbertindjomo@gmail.com>
2025-10-31 15:14:51 +0100

    gestion des mails

--- backend/Routes/SendEmailLogin.js
+++ backend/Routes/SendEmailLogin.js
@@ -55,14 +55,16 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
+  connectionTimeout: 10000,
+  logger: true,
+  debug: true,
 });
+
 
 // Fonction pour envoyer un email à une seule adresse
 export async function sendEmailNotification(destinataire) {
