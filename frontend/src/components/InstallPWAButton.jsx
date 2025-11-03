7e40b9714df65f89246ba0a1d0feaeec8475554f
berol24 <berolbertindjomo@gmail.com>
2025-11-03 10:06:25 +0100

    arrangement bouton installation

--- frontend/src/components/InstallPWAButton.jsx
+++ frontend/src/components/InstallPWAButton.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import download from "../assets/icons/download.png";
 import useInstallPrompt from '../hooks/UseInstallPrompt.jsx';
+import { createPortal } from 'react-dom';
+
 function InstallPWAButton() {
   const { deferredPrompt, isVisible,setDeferredPrompt } = useInstallPrompt();
   const [show, setShow] = useState(true);
@@ -24,14 +26,15 @@ function InstallPWAButton() {
     }
   };
 
-  return (
+  return createPortal(
     <button
       onClick={handleInstallClick}
-      className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-b-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-1 flex items-center justify-between gap-2 w-[80%] md:w-auto z-100"
+      className="fixed top-0 left-1/2 transform -translate-x-1/2 bg-white text-black px-4 py-2 rounded-b-lg shadow-lg transition-transform duration-300 ease-in-out hover:-translate-y-1 flex items-center justify-between gap-2 w-[80%] md:w-auto z-9999 md:z-9999"
     >
       Installer
       <img src={download} alt="download_installation" className="w-5 h-5" />
-    </button>
+    </button>,
+  document.body
   );
 }
 
